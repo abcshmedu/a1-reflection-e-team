736924ee316db2c56b80d6b6f8d8e8e0c19fd3ac
Andrea Limmer <andrea-limmer@gmx.de>
2017-04-05 22:22:01 +0200

    some simple test

--- src/test/java/edu/hm/wgabler/limmer/RendererTest.java
+++ src/test/java/edu/hm/wgabler/limmer/RendererTest.java
@@ -6,11 +6,46 @@
  */
 package edu.hm.wgabler.limmer;
 
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import edu.hm.wgabler.limmer.reflection.RenderMe;
+import edu.hm.wgabler.limmer.reflection.Renderer;
+
 /**
  * @author Wolfgang Gabler, wgabler@hm.edu
  * @since 27/03/2017
  */
 public class RendererTest {
+	
+	private SomeIntClass toRender;
+	private Renderer renderer;
 
+	static class SomeIntClass {
+
+        @RenderMe
+        private int foo;
+        @RenderMe
+        private int bar = 42;
+        
+        private int ignoreThisInt = 1;
+
+        public SomeIntClass(int foo) {
+            this.foo = foo;
+        }
+    }
+	
+	@Test 
+	public void testIntRendering() throws Exception {
+		toRender = new SomeIntClass(22);
+		renderer = new Renderer(toRender);
+		final String expected = "Instance of edu.hm.wgabler.limmer.RendererTest.SomeClass\n" +
+				"foo (Type int): 22\n" +
+				"bar (Type int): 42\n";
+		assertEquals(expected, renderer.render());
+	}
 
 }
