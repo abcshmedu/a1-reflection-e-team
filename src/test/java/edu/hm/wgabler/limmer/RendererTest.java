dd528b26a8cd29cfa9f327493cbffba6759cf623
Andrea Limmer <andrea-limmer@gmx.de>
2017-04-06 13:05:47 +0200

    javadoc for tests

--- src/test/java/edu/hm/wgabler/limmer/RendererTest.java
+++ src/test/java/edu/hm/wgabler/limmer/RendererTest.java
@@ -1,8 +1,8 @@
 /*
- * Wolfgang Gabler
+ * Andrea Limmer
  *
- * Software: Mac OS X 10.12, Oracle Java 1.8.0_111 SE
- * System: Intel Core i7-4850HQ, 16 GByte RAM
+ * Software: Windows 10 Home, Oracle Java 1.8.0_60 SE
+ * System: Intel Atom x7-Z8700, 4 GByte RAM
  */
 package edu.hm.wgabler.limmer;
 
@@ -22,12 +22,19 @@ import edu.hm.wgabler.limmer.reflection.RenderMe;
 import edu.hm.wgabler.limmer.reflection.Renderer;
 
 /**
- * @author Wolfgang Gabler, wgabler@hm.edu
+ * Parameterized Tests for RenderMe Reflection.
+ * 
+ * @author Andrea Limmer, limmer@hm.edu
  * @since 27/03/2017
  */
 @RunWith(Parameterized.class)
 public class RendererTest {
 	
+	/**
+	 * Create Parameters for the tests.
+	 * 
+	 * @return new Object, expected rendered String
+	 */
 	@Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
@@ -41,7 +48,9 @@ public class RendererTest {
                 	 new SomeIntClass(1),
                 	 "Instance of edu.hm.wgabler.limmer.RendererTest.SomeIntClass\n" +
              				"bar (Type double): 42.0\n" +
-             				"foo (Type int): 1\n"
+             				"foo (Type int): 1\n" +
+             				"Method returnDouble (Type double): 88.8\n" +
+             				"Method returnInt (Type int): 99\n" 
                  },
                  {
                 	 new SomeMethodClass('a'),
@@ -59,13 +68,24 @@ public class RendererTest {
            });
     }
     
-    @Parameter // first value of given parameters
+    /**
+     * First value of the provided parameters.
+     * An Object which should be rendered.
+     */
+    @Parameter
     public Object objectToRender;
 
+    /**
+     * Second parameter.
+     * String that is expected to be rendered with the given object.
+     */
     @Parameter(1)
     public String expectedRendering;
 
-
+    /**
+     * Parameterized Test which tests the RenderMe Reflection.
+     * @throws Exception
+     */
 	@Test 
 	public void testRendering() throws Exception {
 		Renderer renderer = new Renderer(objectToRender);
@@ -73,13 +93,14 @@ public class RendererTest {
 	}
 	
 	
-    
+    /**
+     * SomeClass to test basic rendering.
+     */
 	static class SomeClass {
 
-        /*@RenderMe(with = "edu.hm.renderer.ArrayRenderer")
-        int[] array = {1, 2, 3,};*/
         @RenderMe
         private int foo;
+        
         @RenderMe
         private Date date = new Date(123456789);
         
@@ -90,6 +111,9 @@ public class RendererTest {
         }
     }
 	
+	/**
+	 * SomeMethodClass to test rendering of methods.
+	 */
 	static class SomeMethodClass {
 
         @RenderMe
@@ -109,19 +133,34 @@ public class RendererTest {
         	return "blahblahblah";
         }
         
+        /**
+         * Should not be rendered.
+         * @return 0
+         */
         public int doNotRenderMethod() {
         	return 0;
         }
         
-        @RenderMe	// should not render
+        /**
+         * Should not be rendered as method has parameter.
+         * @param input some char
+         * @return given input
+         */
+        @RenderMe	
         public char returnSomeCharWithParam(char input) {
         	return input;
         }
         
-        @RenderMe	// should not render
+        /**
+         * Should not be rendered as return type is void.
+         */
+        @RenderMe
         public void noReturnValue() { }
     }
 	
+	/**
+	 * SomeIntClass to test rendering for int & double.
+	 */
 	static class SomeIntClass {
 
         @RenderMe
@@ -132,43 +171,61 @@ public class RendererTest {
         public SomeIntClass(int foo) {
             this.foo = foo;
         }
+        
+        @RenderMe
+        private int returnInt() {
+        	return 99;
+        }
+        
+        @RenderMe
+        private double returnDouble() {
+        	return 88.8;
+        }
+        
+        /**
+         * Should not be rendered as return type is void.
+         */
+        @RenderMe
+        private void doNotRenderVoid() { }
     }
 	
+	/**
+	 * SomeArrayClass to test rendering for int-arrays.
+	 */
 	static class SomeArrayClass {
 		
+		/**
+		 * Render with specified ArrayRenderer.
+		 */
 		@RenderMe(with="edu.hm.wgabler.limmer.reflection.ArrayRenderer")
-		int[] intArray = {1, 2, 3};
+		int[] intArray;
 		
-		// do not render
+		/**
+		 * Should not be rendered as there is no "@RenderMe".
+		 */
 		int[] doNotRender = {4, 5, 6};
 				
 		public SomeArrayClass(int[] array) {
 			intArray = array;
 		}
 		
+		/**
+		 * Render with ArrayRenderer.
+		 * @return int-array
+		 */
 		@RenderMe(with="edu.hm.wgabler.limmer.reflection.ArrayRenderer")
 		int[] returnIntArray() {
 			return new int[] {11, 22, 33};
 		}
 		
-		// do not render
+		/**
+		 * Do not render as there is no "@RenderMe".
+		 * @return int-array
+		 */
 		int[] doNotRenderIntArray() {
 			return new int[] {44, 55, 66};
 		}
 	}
-	
-	
-	/*@Test 
-	public void testMethodRendering() {
-		renderer = new Renderer(new SomeClass(3));
-		assertEquals(
-				"Instance of edu.hm.wgabler.limmer.RendererTest.SomeClass\n" +
-         				"foo (Type int): 3\n" +
-         				"date (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n" +
-         				"method ...", 
-				renderer.render());
-		
-	}*/
 	
 
 }
